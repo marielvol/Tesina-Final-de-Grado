32785ada4fecd1de961d9be86b0616cc57559080
marielvol <57373454+marielvol@users.noreply.github.com>
2023-09-14 13:26:42 -0300

    Herramienta de Visualización (Prototipo 2)

--- cancer_detection_app/index.php
+++ cancer_detection_app/index.php
@@ -2,12 +2,68 @@
 <html>
 <head>
     <title>Detección de Cáncer de Colon</title>
+    <style>
+        h1 {
+            text-align: left;
+        }
+
+        form {
+            text-align: left;
+        }
+
+        input[type="file"] {
+            display: none;
+        }
+
+        .button-container {
+            display: flex;
+            justify-content: space-between;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+
+        .custom-button {
+            background-color: orange;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+        }
+
+        .custom-button:hover {
+            background-color: darkorange;
+        }
+
+        #image-preview {
+            max-width: 100%;
+            display: none;
+        }
+    </style>
+    <script>
+        function previewImage(input) {
+            var preview = document.getElementById('image-preview');
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    preview.src = e.target.result;
+                    preview.style.display = 'block';
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    </script>
 </head>
 <body>
     <h1>Detección de Cáncer de Colon</h1>
     <form action="upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="uploaded_image">
-        <input type="submit" value="Cargar y Procesar">
+        <label for="uploaded_image" class="custom-button">Seleccionar Archivo</label>
+        <input type="file" name="uploaded_image" id="uploaded_image" style="display:none;" onchange="previewImage(this);">
+        <div class="button-container">
+            <input type="submit" value="Cargar y Procesar" class="custom-button">
+        </div>
+        <img id="image-preview" alt="Vista Previa de la Imagen" src="">
     </form>
 </body>
 </html>
